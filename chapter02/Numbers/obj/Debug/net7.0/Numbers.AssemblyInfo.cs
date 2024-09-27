817784bc9a2ea3eb900cd58c603f1998b3407113
Israa Ezzat <eisraa76@gmail.com>
2024-09-27 20:11:27 +0300

    variables and formatting

--- chapter02/Numbers/obj/Debug/net7.0/Numbers.AssemblyInfo.cs
+++ chapter02/Numbers/obj/Debug/net7.0/Numbers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc682f0ff19df6f6dc52d0e43e799accdc114dfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
