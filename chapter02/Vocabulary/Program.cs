ef1ff55f9c8fd97676b59bec7827a272c20055c0
Israa Ezzat <eisraa76@gmail.com>
2024-09-27 16:13:23 +0300

    vocublary examples

--- chapter02/Vocabulary/Program.cs
+++ chapter02/Vocabulary/Program.cs
@@ -1,15 +1,48 @@
 ﻿
 int subtotal = 0, salesTax = 0;
 var totalPrice = subtotal/*for this item */ + salesTax;
-namespace basics
-{
-    class program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
 
-        }
-    }
-}
+double heightInMetres = 1.88;
+Console.WriteLine($"The variable {nameof(heightInMetres)} has the value{heightInMetres}.");
+
+
+/*string*/
+
+
+
+
+char letter = 'A'; // assigning literal characters
+char digit = '1';
+char symbol = '$';
+
+
+
+
+string firstName = "Bob"; // assigning literal strings
+string lastName = "Smith";
+string phoneNumber = "(215) 555-4256";
+string horizontalLine = new('-', count: 74);
+string grinningEmoji = char.ConvertFromUtf32(0x1F600);
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string grinningEmoji2 = char.ConvertFromUtf32(0x1F600);
+Console.WriteLine(grinningEmoji2);
+
+Console.WriteLine("Bob\\Smith");
+
+Console.WriteLine(@"C:\televisions\sony\bravia.txt");
+Console.WriteLine($$"""
+ {
+ "phone": "{{phoneNumber}}",
+ "age": {{4}},
+ "calculation", "{{{1 + 2}}}"
+ }
+ """);
+Console.WriteLine(""""""""""""
+ """""""""<person age="50">
+ <first_name>Mark</first_name>
+ </person>
+ """"""""""""
+);
+
 
--- chapter02/example3/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿
-double heightInMetres = 1.88;
-Console.WriteLine($"The variable {nameof(heightInMetres)} has the value{ heightInMetres}.");
