3886be26e342b9e8ab53112a2fa04459da3e2d3a
Israa Ezzat <eisraa76@gmail.com>
2024-09-27 20:21:02 +0300

    numbers

---
+++ chapter02/formatting/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+WriteLine(format: "{0} apples cost {1:C}",
+                 arg0: numberOfApples,
+                 arg1: pricePerApple * numberOfApples);
+string formatted = string.Format(format: "{0} apples cost {1:C}",
+                 arg0: numberOfApples,
+                 arg1: pricePerApple * numberOfApples);
+
+
+
+
+ const string firstname = "Omar";
+ const string lastname = "Rudberg";
+ string fullname = firstname + " " + lastname;
+
+Console.WriteLine($"{firstname}{lastname}");
+string? nullString=Console.ReadLine();
+string x = nullString!;
+Console.WriteLine($"{nullString}{lastname}");
---
+++ chapter02/variables/Program.cs
@@ -0,0 +1,19 @@
+﻿object height = 1.88; // storing a double in an object 
+object name = "Amir"; // storing a string in an object
+Console.WriteLine($"{name} is {height} metres tall.");
+//int length1 = name.Length;
+int length2 = ((string)name).Length; // tell compiler it is a string
+Console.WriteLine($"{name} has {length2} characters.");
+dynamic x = 1;
+x = "ahmed";
+x = 0.3M;
+
+
+var population = 67_000_000; // 67 million in UK
+var weight = 1.88; // in kilograms
+var price = 4.99M; // in pounds sterling
+var fruit = "Apples"; // strings use double-quotes
+var letter = 'Z'; // chars use single-quotes
+
+
+var happy = true;
