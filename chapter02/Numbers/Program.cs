ab263c300ee0e2d26c0d36e8ca1695fefa2e08f4
Israa Ezzat <eisraa76@gmail.com>
2024-09-27 16:54:39 +0300

    dealing with numbers

--- chapter02/Numbers/Program.cs
+++ chapter02/Numbers/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿uint naturalNumber = 23;
+
+int integerNumber = -23;
+
+float realNumber = 2.3F;
+
+double anotherRealNumber = 2.3;
+
+
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+
+{
+    double x = 0.1, y = 0.2;
+    Console.WriteLine($"{x+y == 0.3}");
+}
+{ 
+decimal x = 0.1M, y = 0.2M;
+Console.WriteLine($"{x+y == 0.3M}");}
