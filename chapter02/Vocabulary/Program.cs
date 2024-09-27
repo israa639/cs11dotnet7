63c06e90213a2db9360bc4d123b30c859bc538e7
Israa Ezzat <eisraa76@gmail.com>
2024-09-27 14:06:05 +0300

    first 3 examples

---
+++ chapter02/Vocabulary/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int subtotal = 0, salesTax = 0;
+var totalPrice = subtotal/*for this item */ + salesTax;
+namespace basics
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
+
---
+++ chapter02/example3/Program.cs
@@ -0,0 +1,3 @@
+﻿
+double heightInMetres = 1.88;
+Console.WriteLine($"The variable {nameof(heightInMetres)} has the value{ heightInMetres}.");
