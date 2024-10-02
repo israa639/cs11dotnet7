37ec1df759b18abfa465e80f723745562539ff7d
Israa Ezzat <eisraa76@gmail.com>
2024-10-02 17:50:54 +0300

    debugging

---
+++ chapter04/Instrumenting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+
+
+string logPath = Path.Combine(Environment.GetFolderPath( Environment.SpecialFolder.DesktopDirectory), "log.txt");
+Console.WriteLine($"Writing to: {logPath}");
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+Trace.Listeners.Add(logFile);
+// text writer is buffered, so this option calls
+// Flush() on all listeners after writing
+Trace.AutoFlush = true;
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
