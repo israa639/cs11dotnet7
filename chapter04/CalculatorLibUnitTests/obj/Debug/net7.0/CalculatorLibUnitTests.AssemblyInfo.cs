fcaba2ba469356cceb55167aad50e826a1636b77
Israa Ezzat <eisraa76@gmail.com>
2024-10-03 11:00:06 +0300

    Exception part

--- chapter04/CalculatorLibUnitTests/obj/Debug/net7.0/CalculatorLibUnitTests.AssemblyInfo.cs
+++ chapter04/CalculatorLibUnitTests/obj/Debug/net7.0/CalculatorLibUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorLibUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc741021a48d822a128a2e56ad6dd2e3f08f9c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorLibUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorLibUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ chapter04/CallStackExceptionHandlingLib/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace CallStackExceptionHandlingLib
+{
+    public class Calculator
+    {
+        public static void Gamma()
+        {
+           Console.WriteLine("In Gamma");
+            Delta();
+        }
+        private static void Delta()
+       
+        {
+            Console.WriteLine("In Delta");
+            File.OpenText("bad file path");
+        }
+    }
+}
