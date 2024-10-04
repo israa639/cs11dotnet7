d3a421c061f325bdf8f6cb07b2e40d00bfeb0e3d
Israa Ezzat <eisraa76@gmail.com>
2024-10-04 21:03:19 +0300

    chapter05 initial commit

---
+++ PacktLibraryNetStandard2/Chapter05/Chapter05/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Packt.Shared;
+
+public class Person
+{
+   
+    public string? Name;
+    public DateTime DateOfBirth;
+    public WondersOfTheAncientWorld FavoriteAncientWonder;
+    public readonly string HomePlanet;
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+    public string GetOrigin()
+    {
+        return $"{Name} was born on {HomePlanet}.";
+    }
+
+}
---
+++ PacktLibraryNetStandard2/Chapter05/Chapter05/WondersOfTheAncientWorld.cs
@@ -0,0 +1,11 @@
+﻿namespace Packt.Shared;
+public enum WondersOfTheAncientWorld
+{
+    GreatPyramidOfGiza,
+    HangingGardensOfBabylon,
+    StatueOfZeusAtOlympia,
+    TempleOfArtemisAtEphesus,
+    MausoleumAtHalicarnassus,
+    ColossusOfRhodes,
+    LighthouseOfAlexandria
+}
