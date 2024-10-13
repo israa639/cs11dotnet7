4330ad7c963856114c43dd944f5beeeec8197fba
Israa Ezzat <eisraa76@gmail.com>
2024-10-13 13:57:47 +0300

    working with regex

---
+++ Chapter08/WorkingWithRegularExpressions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using static System.Console;
+Write("Enter your age: ");
+string input = ReadLine()!; // null-forgiving
+Regex ageChecker = new(@"^[A-Z]{2}\d{3}$");
+if (ageChecker.IsMatch(input))
+{
+    WriteLine("Thank you!");
+}
+else
+{
+    WriteLine($"This is not a valid age: {input}");
+}
+
+
+string films = """
+"Monsters, Inc.","I, Tonya","Lock, Stock and Two Smoking Barrels"
+""";
+WriteLine($"Films to split: {films}");
+string[] filmsDumb = films.Split(',');
+WriteLine("Splitting with string.Split method:");
+foreach (string film in filmsDumb)
+{
+    WriteLine(film);
+}
