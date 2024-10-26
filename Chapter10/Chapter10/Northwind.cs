92517f971c4bc52001817bac5c8c8f011cc8dbb4
Israa Ezzat <eisraa76@gmail.com>
2024-10-26 19:59:58 +0300

    chapter 10 init commit

---
+++ Chapter10/Chapter10/Northwind.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Packt.Shared;
+
+public class Northwind : DbContext
+{
+    // these properties map to tables in the database
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+    protected override void OnConfiguring(
+      DbContextOptionsBuilder optionsBuilder)
+    { }
+    protected override void OnModelCreating(
+ ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Category>()
+          .Property(category => category.CategoryName)
+          .IsRequired() // NOT NULL
+          .HasMaxLength(15);
+        if (Database.ProviderName?.Contains("Sqlite") ?? false)
+        {
+            modelBuilder.Entity<Product>()
+              .Property(product => product.Cost)
+              .HasConversion<double>();
+        }
+    }
+}
